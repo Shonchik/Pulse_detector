e2040ae6cb10e9e2a76503a1de2281d9edc4a5b8
funny-family <maksvadik228@gmail.com>
2022-05-06 16:51:21 +0700

    complite base setup

---
+++ project/client/src/app/app.component.tsx
@@ -0,0 +1,7 @@
+import '../styles/main.css';
+import { FC } from 'react';
+import styles from './app.module.css';
+
+export const App: FC = () => {
+  return <div className={styles.app}>Hello from react</div>;
+};
