f7b4eba87a279111d667d8b996448eb3e2cfcdab
funny-family <maksvadik228@gmail.com>
2022-05-06 17:04:23 +0700

    add fallback if "navigator.mediaDevices" is not supported

--- project/client/src/app/app.component.tsx
+++ project/client/src/app/app.component.tsx
@@ -1,7 +1,18 @@
 import '../styles/main.css';
 import { FC } from 'react';
 import styles from './app.module.css';
+import { FullScreenOverlay } from './components/full-screen-overlay/full-screen-overlay.component';
 
 export const App: FC = () => {
-  return <div className={styles.app}>Hello from react</div>;
+  return (
+    <div className={styles.app}>
+      {navigator.mediaDevices === undefined ? (
+        <FullScreenOverlay>
+          Приложение не поддерживает &quot;mediaDevices&quot; API.
+        </FullScreenOverlay>
+      ) : (
+        <div>Kek</div>
+      )}
+    </div>
+  );
 };
