54f17a8dc774a29f18e1b198447ffd8906c5e2af
Shonchik <31989755+Shonchik@users.noreply.github.com>
2022-05-06 11:13:00 +0700

    Add files via upload

--- project/api.py
+++ project/api.py
@@ -20,29 +20,34 @@ api = Api(app)
 class getPulseApp():
 
     def __init__(self):
-        self.port = 8000
+        self.port = 8001
         self.processor = findFaceGetPulse(bpm_limits=[50, 160], data_spike_limit=2500., face_detector_smoothness=10.)
+        # self.loop = asyncio.get_event_loop()
 
-    async def handler(websocket, path):
-	    data = await websocket.recv()
-	    answer = main_loop(data)
-	    await websocket.send(answer)
+    async def handler(self, websocket, path):
+        while True:
+            data = await websocket.recv()
+            answer = self.main_loop(data)
+            await websocket.send(answer)
 
-    def main_loop(self, frame):
+    async def main_loop(self, frame):
         self.processor.frame_in = frame
         self.processor.run(1)
         return str(self.processor.bpm)
 
-    def run(self):
-        start_socket = websockets.serve(self.handler, "localhost", self.port)
-        asyncio.get_event_loop().run_until_complete(start_socket)
-        asyncio.get_event_loop().run_forever()
-
+    async def run(self):
+        # start_socket = websockets.serve(self.handler, "localhost", self.port)
+        # self.loop.run_until_complete(start_socket)
+        # self.loop.run_forever()
+        print("kek")
+        async with websockets.serve(self.handler, "localhost", self.port):
+            await asyncio.Future()
+        
+AppPulse = getPulseApp()
+            
 class Pulse(Resource):
     def get(self, id=0):
         if id == 0:
-            AppPulse = getPulseApp()
-            AppPulse.run()	
             return AppPulse.port, 200
         return "pulse not found", 404
 
@@ -50,6 +55,7 @@ api.add_resource(Pulse, "/pulse")
 
 if __name__ == '__main__':
     app.run(debug=True)
+    asyncio.run(AppPulse.run())
 
 
 
