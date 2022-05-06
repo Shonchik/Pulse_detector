760ec300bc2140bd506c16da4141c831cc9bb203
Shonchik <31989755+Shonchik@users.noreply.github.com>
2022-05-06 15:01:55 +0700

    Add files via upload

--- project/api.py
+++ project/api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from flask_socketio import SocketIO, emit
 from lib.device import Camera
 from lib.processors_noopenmdao import findFaceGetPulse
 from lib.interface import plotXY, imshow, waitKey, destroyWindow
@@ -17,45 +18,47 @@ import websockets
 app = Flask(__name__)
 api = Api(app)
 
+sessions = []
+
 class getPulseApp():
 
     def __init__(self):
-        self.port = 8001
         self.processor = findFaceGetPulse(bpm_limits=[50, 160], data_spike_limit=2500., face_detector_smoothness=10.)
-        # self.loop = asyncio.get_event_loop()
 
-    async def handler(self, websocket, path):
-        while True:
-            data = await websocket.recv()
-            answer = self.main_loop(data)
-            await websocket.send(answer)
-
-    async def main_loop(self, frame):
+    def run(self, frame):
         self.processor.frame_in = frame
         self.processor.run(1)
+
+    def get_bpm(self):
         return str(self.processor.bpm)
-
-    async def run(self):
-        # start_socket = websockets.serve(self.handler, "localhost", self.port)
-        # self.loop.run_until_complete(start_socket)
-        # self.loop.run_forever()
-        print("kek")
-        async with websockets.serve(self.handler, "localhost", self.port):
-            await asyncio.Future()
-        
-AppPulse = getPulseApp()
             
-class Pulse(Resource):
-    def get(self, id=0):
-        if id == 0:
-            return AppPulse.port, 200
-        return "pulse not found", 404
+AppPulse = getPulseApp()
 
-api.add_resource(Pulse, "/pulse")
+class Session(Resource):
+    def get(self):
+        id = len(sessions)
+        app = getPulseApp()
+        sessions.append(app)
+        return id, 200
+
+class NewData(Resource):
+    def get(self, id=-1, frame = ""):
+        app = sessions[id]
+        app.run(frame)
+        return "All good", 200
+
+class BPM(Resource):
+    def get(self, id=0):
+        app = sessions[id]
+        print(app.get_bpm())
+        return app.get_bpm(), 200
+
+api.add_resource(Session, "/new_session")
+api.add_resource(NewData, "/new_data/<int:id>/<string:frame>")
+api.add_resource(BPM, "/get_bpm/<int:id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
-    asyncio.run(AppPulse.run())
 
 
 
