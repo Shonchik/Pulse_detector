1f7913bc4cc423421b4c2db5fa4452b48442c930
funny-family <maksvadik228@gmail.com>
2022-05-06 17:04:23 +0700

    add fallback if "navigator.mediaDevices" is not supported

---
+++ project/client/src/app/components/full-screen-overlay/full-screen-overlay.component.tsx
@@ -0,0 +1,27 @@
+import { DetailedHTMLProps, HTMLAttributes, FC } from 'react';
+import styles from './full-screen-overlay.module.css';
+
+type FullScreenOverlayAttrs = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+
+type FullScreenOverlayProps = {
+  //
+};
+
+type FullScreenOverlayAttrsAndProps = FullScreenOverlayAttrs &
+  FullScreenOverlayProps;
+
+export const FullScreenOverlay: FC<FullScreenOverlayAttrsAndProps> = ({
+  children,
+  ...attrs
+}) => {
+  const className = `${attrs.className} ${styles.fullScreenOverlay}`;
+
+  return (
+    <div {...attrs} className={className}>
+      {children}
+    </div>
+  );
+};
